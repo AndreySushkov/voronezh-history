45cfb5471fc903967683681e0e3e2e518cf4f383
AndreySushkov <asushkov2020@gmail.com>
2024-05-12 18:41:00 +0300

    some fixes and favicon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// анимация хедера
 const header = document.querySelector('.header')
 
 window.addEventListener('scroll', function() {
@@ -10,6 +11,7 @@ window.addEventListener('scroll', function() {
   }
 })
 
+// меню-бургер
 const html = document.querySelector('html')
 const hamburgerBtn = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav__list')
@@ -19,3 +21,21 @@ hamburgerBtn.addEventListener('click', function() {
   hamburgerBtn.classList.toggle('hamburger--active')
   navList.classList.toggle('nav__list--active')
 })
+
+// валидация формы
+const contactsForm = document.querySelector('.contacts__form')
+const contactsCheckbox = document.querySelector('.contacts__form-checkbox')
+const contactsSubmit = document.querySelector('.contacts__form-btn')
+
+if (contactsSubmit) {
+  contactsSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (contactsForm.reportValidity()) {
+      if (!contactsCheckbox.checked) {
+        alert('Вы не поставили галочку возле поля на согласие на обработку персональных данных!')
+      } else {
+        contactsForm.submit()
+      }
+    }
+  })
+}
